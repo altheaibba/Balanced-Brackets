89644aeb4e77aaa8b86f7423430b219908346759
altheaibba <32885017+altheaibba@users.noreply.github.com>
2018-09-13 13:47:25 +0100

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,47 @@
+package prac;
+
+import java.util.*;
+
+public class Stack
+{
+    int size;
+    int top;
+    String [] array;
+    
+    public Stack(int size)
+    {
+       array = new String [size];
+       top = 0;
+    }
+    
+    public void push(String s)
+    {
+        array[top] = s;
+        top++;
+    }
+    
+    public String pop()
+    {
+    	String temp = array[top-1];
+        array[top-1] = null;
+        top--;
+        return temp;
+    }
+    
+    public String peek()
+    {
+        return array[top-1];
+    }
+    
+    public boolean isEmpty()
+    {
+        if(top==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ balancedBrackets.java
@@ -0,0 +1,49 @@
+package prac;
+
+import java.util.*;
+
+public class balancedBrackets {
+
+	public static void main (String [] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        Stack myStack = new Stack(6);
+        String str = scan.nextLine();
+        
+        for(int i = 0; i<str.length(); i++)
+        {
+        	String input = Character.toString(str.charAt(i));
+        	
+        	if(input.equals("(") | input.equals("{") | input. equals("[")) //If the char is an opening bracket,
+            {
+                myStack.push(input); //then push the opening bracket onto the stack
+            }
+            else
+            {
+                if(input.equals(")") && (myStack.peek()).equals("(")) //If the char is a closing bracket and the char at the top of the stack is its matching opening,
+                {
+                    myStack.pop(); //then pop its matching opening bracket from the stack
+                }
+                else if(input.equals("}") && (myStack.peek()).equals("{"))
+                {
+                    myStack.pop();
+                }
+                else if(input.equals("]") && (myStack.peek()).equals("["))
+                {
+                    myStack.pop();
+                }
+            }
+        }
+        
+        if(myStack.isEmpty()) //If the stack is empty
+        {
+        	System.out.println("true"); //All brackets in the String is balanced
+        }
+        else
+        {
+        	System.out.println("false");
+        }
+        
+    }
+}
